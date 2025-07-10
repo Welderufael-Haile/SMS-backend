85cc8c6468bcf89ada53bd01f9c22dfc3238e354
welderufaelhaile <rufaelhaile14@gmail.com>
2025-07-10 17:02:54 +0300

    add all updated file

--- config/db.js
+++ config/db.js
@@ -2,10 +2,11 @@ const mysql = require('mysql2');
 require('dotenv').config(); // Load environment variables
 
 const pool = mysql.createPool({
-  host: process.env.DB_HOST || 'localhost',
-  user: process.env.DB_USER || 'root',
-  password: process.env.DB_PASSWORD || '', // Empty for XAMPP
-  database: process.env.DB_NAME || 'school_management',
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD, // Empty for XAMPP
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT || 3306,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
